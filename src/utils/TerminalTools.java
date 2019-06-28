3180f85e12c8e3f7272deee0342e529509ab8c74
YangLang <yangl@inke.cn>
2019-06-28 16:14:22 +0800

    更新terminal执行方式

--- src/utils/TerminalTools.java
+++ src/utils/TerminalTools.java
@@ -1,9 +1,11 @@
 package utils;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.terminal.JBTerminalPanel;
+import com.jediterm.terminal.TerminalOutputStream;
 
 import javax.swing.*;
 
@@ -11,11 +13,24 @@ public class TerminalTools {
 
     public static void sendCmd(Project project, String cmd) {
         ToolWindow terminal = ToolWindowManager.getInstance(project).getToolWindow("Terminal");
-        JComponent root = terminal.getComponent();
         terminal.show(() -> {
-            JBTerminalPanel terminalPanel = ((JBTerminalPanel) ((JLayeredPane) ((JPanel) ((JPanel) ((JPanel) ((JPanel) ((JPanel) ((JPanel) root.getComponent(0)).getComponent(0)).getComponent(0)).getComponent(0)).getComponent(0)).getComponent(0)).getComponent(0)).getComponent(0));
-            //执行history命令
-            terminalPanel.getTerminalOutputStream().sendString(cmd + "\n");
+            JComponent currentComponent = terminal.getComponent();
+            while (currentComponent != null) {
+                if (currentComponent instanceof JBTerminalPanel) {
+                    TerminalOutputStream terminalOutputStream = ((JBTerminalPanel) currentComponent).getTerminalOutputStream();
+                    if (terminalOutputStream == null) {
+                        ToastUtil.make(project, MessageType.INFO, "首次创建终端对象，请重新执行");
+                    } else {
+                        terminalOutputStream.sendString(cmd + "\n");
+                    }
+                    break;
+                }
+                int componentCount = currentComponent.getComponentCount();
+                if (componentCount <= 0) {
+                    break;
+                }
+                currentComponent = (JComponent) currentComponent.getComponent(0);
+            }
         });
     }
 }
